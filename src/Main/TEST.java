d54b1857699a09a912cc5116fce6799c265543d7
TruongIKPK <149455408+TruongIKPK@users.noreply.github.com>
2024-11-10 00:32:58 +0700

    Truong2-11/10

--- src/Main/TEST.java
+++ src/Main/TEST.java
@@ -12,19 +12,20 @@ public class TEST {
 		ThuatToanRSA nguoi1 = new ThuatToanRSA();
 		ThuatToanRSA nguoi2 = new ThuatToanRSA();
 		
-		nguoi1.khoitao();
-		nguoi2.khoitao();
-		
 		String thongdiepChuoi = "1"; 
 		
 		BigInteger thongdiep = new BigInteger(thongdiepChuoi.getBytes(StandardCharsets.UTF_8));
 		
 		BigInteger maHoa = nguoi1.maHoa(thongdiep, nguoi2.getN());
+		String maHoaChuoi = new String(maHoa.toByteArray(), StandardCharsets.UTF_8);
 		
 		BigInteger dagiai = nguoi2.giaiMa(maHoa);
+		
 		String dagiaiChuoi = new String(dagiai.toByteArray(), StandardCharsets.UTF_8);
+		
 		System.out.println(thongdiepChuoi);
 		System.out.println(maHoa);
+		System.out.println(maHoaChuoi);
 		System.out.println(dagiaiChuoi);
 		
 	}
--- src/ThuatToan/ThuatToanRSA.java
+++ src/ThuatToan/ThuatToanRSA.java
@@ -10,13 +10,13 @@ public class ThuatToanRSA {
 	private BigInteger q;
 	private BigInteger n;
 	private BigInteger phiN;
-	//Verson 2.0
+	// ->> Version 2.0
 	private BigInteger dP;
 	private BigInteger dQ;
 	private BigInteger qInv;
-	
-	
-	public void khoitao() {
+	// <<-
+	public ThuatToanRSA() {
+		// TODO Auto-generated constructor stub
 		BigInteger checkTruoc = BigInteger.TWO.pow(VERSION - 1);
 		BigInteger checkSau = BigInteger.TWO.pow(VERSION);     
 		do {
@@ -24,10 +24,12 @@ public class ThuatToanRSA {
 			q = BigInteger.probablePrime(VERSION/2, new Random());
 			n = p.multiply(q);
 		}while( n.compareTo(checkTruoc) <= 0 || n.compareTo(checkSau) >= 0);
+		
 		phiN = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
-		
-		//d = E.modInverse(phiN); Verson 1.0
-		
+		// ->> Version 1.0
+		//d = E.modInverse(phiN);
+		// <<- 
+		// ->> Version 2.0
 		dP = E.modInverse(p.subtract(BigInteger.ONE)); 
 	 	dQ = E.modInverse(q.subtract(BigInteger.ONE)); 
 
@@ -36,16 +38,21 @@ public class ThuatToanRSA {
 		}else {
 			qInv = p.modInverse(q);
 		}
+		// <<-
 	}
 	public BigInteger maHoa(BigInteger thongdien, BigInteger pN) {
 		return thongdien.modPow(E, pN);
 	}
 	public BigInteger giaiMa(BigInteger maDAHOA) {
-//		return maDAHOA.modPow(d, n); Verson 1.0
+		// ->> Version 1.0
+		// return maDAHOA.modPow(d, n);
+		// <<- 
+		// ->> Version 2.0
 		BigInteger m1 = maDAHOA.modPow(dP, p);
 		BigInteger m2 = maDAHOA.modPow(dQ, q);
 		BigInteger h = (qInv.multiply(m1.subtract(m2))).mod(p);
 		return m2.add(q.multiply(h));
+		// <<-
 	}
 	public BigInteger getN() {
 		return n;

5527e22a8bb12444d9bb9c859a3ae4d623e27e25
TruongIKPK <149455408+TruongIKPK@users.noreply.github.com>
2024-11-10 14:21:10 +0700

    Truong_2-11/10

--- src/ThuatToan/ThuatToanRSA.java
+++ src/ThuatToan/ThuatToanRSA.java
@@ -27,31 +27,76 @@ public class ThuatToanRSA {
 		
 		phiN = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
 		// ->> Version 1.0
-		//d = E.modInverse(phiN);
+		qInv = E.modInverse(phiN);
 		// <<- 
 		// ->> Version 2.0
-		dP = E.modInverse(p.subtract(BigInteger.ONE)); 
-	 	dQ = E.modInverse(q.subtract(BigInteger.ONE)); 
-
-		if(p.compareTo(q) > 0) {
-			qInv = q.modInverse(p);
-		}else {
-			qInv = p.modInverse(q);
-		}
+//		dP = E.modInverse(p.subtract(BigInteger.ONE)); 
+//	 	dQ = E.modInverse(q.subtract(BigInteger.ONE)); 
+//
+//		if(p.compareTo(q) > 0) {
+//			qInv = q.modInverse(p);
+//		}else {
+//			qInv = p.modInverse(q);
+//		}
 		// <<-
+	}
+	public BigInteger getP() {
+		return p;
+	}
+	public void setP(BigInteger p) {
+		this.p = p;
+	}
+	public BigInteger getQ() {
+		return q;
+	}
+	public void setQ(BigInteger q) {
+		this.q = q;
+	}
+	public BigInteger getPhiN() {
+		return phiN;
+	}
+	public void setPhiN(BigInteger phiN) {
+		this.phiN = phiN;
+	}
+	public BigInteger getdP() {
+		return dP;
+	}
+	public void setdP(BigInteger dP) {
+		this.dP = dP;
+	}
+	public BigInteger getdQ() {
+		return dQ;
+	}
+	public void setdQ(BigInteger dQ) {
+		this.dQ = dQ;
+	}
+	public BigInteger getqInv() {
+		return qInv;
+	}
+	public void setqInv(BigInteger qInv) {
+		this.qInv = qInv;
+	}
+	public static int getVersion() {
+		return VERSION;
+	}
+	public static BigInteger getE() {
+		return E;
+	}
+	public void setN(BigInteger n) {
+		this.n = n;
 	}
 	public BigInteger maHoa(BigInteger thongdien, BigInteger pN) {
 		return thongdien.modPow(E, pN);
 	}
 	public BigInteger giaiMa(BigInteger maDAHOA) {
 		// ->> Version 1.0
-		// return maDAHOA.modPow(d, n);
+		 return maDAHOA.modPow(qInv, n);
 		// <<- 
 		// ->> Version 2.0
-		BigInteger m1 = maDAHOA.modPow(dP, p);
-		BigInteger m2 = maDAHOA.modPow(dQ, q);
-		BigInteger h = (qInv.multiply(m1.subtract(m2))).mod(p);
-		return m2.add(q.multiply(h));
+//		BigInteger m1 = maDAHOA.modPow(dP, p);
+//		BigInteger m2 = maDAHOA.modPow(dQ, q);
+//		BigInteger h = (qInv.multiply(m1.subtract(m2))).mod(p);
+//		return m2.add(q.multiply(h));
 		// <<-
 	}
 	public BigInteger getN() {
